e50f390774cc1751ab606936c25250beb6c5a8eb
Nicole <128891766+Niffiny@users.noreply.github.com>
2023-04-12 17:39:51 +0500

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import {similarPhoto} from './data.js';
+import {addPhotoMin} from './minPhoto.js';
+import '/userForm.js';
+import '/validatePictForm.js';
+import '/changePhoto.js';
+
+addPhotoMin(similarPhoto);
