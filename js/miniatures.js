f2e78467a2301fa2338762dd91848c81f493d8bc
Nicole <128891766+Niffiny@users.noreply.github.com>
2023-04-22 19:15:25 +0500

    Create miniatures.js

---
+++ js/miniatures.js
@@ -0,0 +1,19 @@
+import { numberPhotoUsers } from './data.js';
+
+const miniaturesFragment = document.createDocumentFragment();
+
+
+//Добавление
+const miniaturesList = document.querySelector('.pictures');
+//Шаблон
+const miniaturesTemplate = document.querySelector('#picture').content;
+
+const simularMiniatures = numberPhotoUsers;
+simularMiniatures.forEach(({url, likes, comments})=>{
+  const miniatures = miniaturesTemplate.cloneNode(true);
+  miniatures.querySelector('.picture__img').src = url;
+  miniatures.querySelector('.picture__likes').textContent=likes;
+  miniatures.querySelector('.picture__comments').textContent=comments.length;
+  miniaturesFragment.appendChild(miniatures);
+});
+miniaturesList.appendChild(miniaturesFragment);
