571fed608ac2426ff08c89d0dbf0454735c06e4b
Nicole <128891766+Niffiny@users.noreply.github.com>
2023-04-23 10:47:05 +0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,103 +1,30 @@
-import './hashtadsvalid.js';
-import { input,hashtagsValid } from './hashtadsvalid.js';
-const imgOverlay = document.querySelector('.img-upload__overlay');
-const start = document.querySelector('.img-upload__start input');
+import {isCorrectLength} from './util.js';
 
-const photoUser = document.querySelector('#upload-file');
+const form = document.querySelector('#upload-select-image');
+const hashtagField = form.querySelector('.text__hashtags');
+const descField = form.querySelector('.text__description');
+const descriptionMaxLength = 140;
+const hashtagsMaxCount = 5;
 
-const body=document.querySelector('body');
-const imgPreview = document.querySelector('.img-upload__preview img');//для замены на пользовательское
-start.onchange = function () {
-  imgOverlay.classList.remove('hidden');
-  body.classList.add('modal-open');
-  imgPreview.src = photoUser.value;
-};
-
-const cancel = document.querySelector('.img-upload__cancel');
-cancel.addEventListener('click', () => {
-  imgOverlay.classList.add('hidden');
-  body.classList.remove('modal-open');
-  photoUser.value ='';
+export const validator = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'img-upload__field-wrapper--invalid',
+  successClass: 'img-upload__field-wrapper--valid',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'span',
+  errorTextClass: 'img-upload__field__error'
 });
+validator.addValidator(hashtagField, validateHashtags, 'Хэштегов может быть не больше 5 через пробел.' +
+  ' Допускаются только буквы и числа.');
+validator.addValidator(descField, validateDescription, 'Максимальное количество символов - 140');
 
-document.addEventListener('keydown', (evt) => {
-  if (evt.key === 'Escape') {
-    evt.preventDefault();
-    imgOverlay.classList.add('hidden');
-    body.classList.remove('modal-open');
-    photoUser.value = '';
-  }
-});
+function validateDescription(value) {
+  return isCorrectLength(value, descriptionMaxLength);
+}
 
-// показ сообщения,что сообщение отправлено БЛОК РАБОТАЕТ КАК НАДО
-const showSuccessMessageModal = () => {
-  const successModal = document.querySelector('#success').content.querySelector('.success');
-  const clonedSuccessModal = successModal.cloneNode(true);
-  const closeSuccessModalButtonElement = clonedSuccessModal.querySelector('.success__button');
-
-  closeSuccessModalButtonElement.addEventListener('click',(evt) =>{
-    evt.preventDefault();
-    body.removeChild(clonedSuccessModal);
-
-  } );
-
-  document.addEventListener('keydown', (evt) => {
-    if (evt.key === 'Escape') {
-      evt.preventDefault();
-      clonedSuccessModal.remove();
-    }
-  });
-
-  document.addEventListener('click',()  => {
-    clonedSuccessModal.remove();
-  });
-  document.body.append(clonedSuccessModal);
-};
-//показ сообщения об ошибке БЛОК РАБОТАЕТ
-const showErrorMessageModal = () =>{
-  const errorModal = document.querySelector('#error').content.querySelector('.error');
-  const clonedErrorModal = errorModal.cloneNode(true);
-  const closeErrorModalButtonElement = clonedErrorModal.querySelector('.error__button');
-  closeErrorModalButtonElement.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    body.removeChild(clonedErrorModal);
-  });
-  document.addEventListener('keydown', (evt) => {
-    if (evt.key === 'Escape') {
-      evt.preventDefault();
-      clonedErrorModal.remove();
-    }
-  });
-  document.addEventListener('click', () => {
-    clonedErrorModal.remove();
-  });
-  document.body.append(clonedErrorModal);
-};
-
-const form = document.querySelector('.img-upload__form');
-const pristine = new Pristine(form);
-pristine.addValidator(document.querySelector('[name="hashtags"]'), hashtagsValid);
-
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  if (input.value==='') {
-    showSuccessMessageModal();
-    imgOverlay.classList.add('hidden');
-    body.classList.remove('modal-open');
-    photoUser.value = '';//позволяет загрузить одну и ту же фотку
-    input.value = '';
-  }
-  else if (pristine.validate()) {
-    showSuccessMessageModal();
-    imgOverlay.classList.add('hidden');
-    body.classList.remove('modal-open');
-    photoUser.value = '';
-    input.value = '';
-  }
-  else {
-    showErrorMessageModal();
-    imgOverlay.classList.add('hidden');
-    body.classList.remove('modal-open');
-    photoUser.value = '';
-  }
-});
+function validateHashtags(value) {
+  const hashtags = value.split(' ').map((v) => v.toLowerCase());
+  return (new Set(hashtags)).size === hashtags.length &&
+    hashtags.length <= hashtagsMaxCount &&
+    hashtags.every((tag) => /^#[а-яa-z0-9]{1,19}}$/.test(tag));
+}
